cac56077b6a49429ccd062fa8edd21e63dead314
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-07-24 23:01:30 -0300

    confining player movement

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -1,46 +1,102 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
+pub const PLAYER_SPEED: f32 = 500.0; // player movement speed
+pub const PLAYER_SIZE: f32 = 64.0; // player sprite size
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
+        .add_systems(Update, player_movement)
+        .add_systems(Update, confine_player_movement)
         .run();
 }
 
 #[derive(Component)]
-pub struct Player {
+pub struct Player {}
 
-}
-
-pub fn spawn_player (
+pub fn spawn_player(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>
-) {   
-    let window: &Window = window_query.get_single().unwrap();
-    commands.spawn(
-        (
-            SpriteBundle {
-                transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0) ,
-                texture: asset_server.load("sprites/ball_blue_large.png"),
-                ..default()
-            },
-            Player {},
-        )
-    );
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
 }
 
-pub fn spawn_camera(
-    mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>
- ){
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window: &Window = window_query.get_single().unwrap();
-    commands.spawn(
-        Camera2dBundle{
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            ..default()
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1.0),
+        ..default()
+    });
+}
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
         }
-        );
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+        let half_player_size: f32 = PLAYER_SIZE / 2.0;
+        let x_min: f32 = half_player_size;
+        let x_max: f32 = window.width() - half_player_size;
+        let y_min: f32 = half_player_size;
+        let y_max: f32 = window.height() - half_player_size;
+
+        let mut translation: Vec3 = player_transform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation;
+    }
 }
