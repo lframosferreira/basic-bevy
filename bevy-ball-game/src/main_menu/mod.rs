106b71a50a5138dbea5ec87756dd8f608757bf7d
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-07-26 19:42:45 -0300

    incrementing UI

--- bevy-ball-game/src/main_menu/mod.rs
+++ bevy-ball-game/src/main_menu/mod.rs
@@ -1,13 +1,18 @@
+mod components;
+mod styles;
+mod systems;
+
+use systems::layout::*;
+
 use bevy::prelude::*;
+
+use crate::AppState;
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, main_menu);
+        app.add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
+            .add_systems(OnExit(AppState::MainMenu), despawn_main_menu);
     }
-}
-
-pub fn main_menu() {
-    println!("You are on the main menu.");
 }
